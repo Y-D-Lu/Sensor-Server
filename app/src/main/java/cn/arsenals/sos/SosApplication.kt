7eccc4faac33b6381116b946eda9be550eaee1ff
Y-D-Lu <e4256318151@gmail.com>
2019-03-16 15:19:57 +0800

    rename

--- app/src/main/java/cn/arsenals/sos/SosApplication.kt
+++ app/src/main/java/cn/arsenals/sos/SosApplication.kt
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 
-class SOSApplication : Application() {
+class SosApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
